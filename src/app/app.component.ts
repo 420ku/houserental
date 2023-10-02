5771e953516c2bc32028e84919754e08e2c7f0ef
420ku <mordecai51213@gmail.com>
2023-10-02 16:44:15 +0800

    angular houserental acitivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './home/home.component.ts',
+  template: ` <main> <header class="brand-name"> <section class="brand-content"><img class="brand-logo" src="/assets/logo.png" alt="logo"
+aria-hidden="true"><h1 style="color:#605dc8">Homes</h1></section> </header> <section class="content"> <app-home></app-home> </section> </main> `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'houserental';
+  title = 'homes';
 }
